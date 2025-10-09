4056b0647b2070bf93e98b72425105450d3bf9b3
smedum <34515940+smedum@users.noreply.github.com>
2025-10-09 16:17:50 +0100

    Create App.jsx
    feat: Add main App component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function App() {
+  return (
+    <div>
+      <h1>Gaelic Language Engine</h1>
+      <p>Your language learning app is running!</p>
+    </div>
+  )
+}
+
+export default App
